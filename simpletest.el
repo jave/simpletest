7b3090aedd6743b69d0190850d73b5f771d6879b
jave <joakim@verona.se>
2013-03-15 01:32:11 +0100

    use emacs widgets instead

--- simpletest.el
+++ simpletest.el
@@ -2,6 +2,7 @@
 ;; joakim verona
 ;; gplv3
 
+(require 'widget)
 
 (defvar mtn-exercise-answer)
 (defvar mtn-current-exercise)
@@ -40,8 +41,8 @@
     (mtn-format-exercise  a "+" b )))
 
 (defun mtn-make-exercise-- ()
-  (let ( (a (random 21))
-         (b (random 11)))
+  (let* ( (a (random 21))
+         (b (random (+ a 1))))
     (setq mtn-exercise-answer  (-  a b))
     (mtn-format-exercise  a "-" b )))
 
@@ -55,19 +56,29 @@
 
 
 (defun mtn-insert-exercise ()
-  (insert
-   (propertize 
-    (concat 
-     (propertize (format "%02d: " mtn-current-exercise) 
-                 'font-lock-face '(:foreground "grey")
-                 )
-     (propertize  (mtn-make-exercise)
-                  'font-lock-face '(:foreground "yellow")
-                  'read-only t                       
-                  ))
-    'read-only t
-    'rear-nonsticky t)
-   ))
+  (widget-insert
+   (propertize  (format "%02d: " mtn-current-exercise )  'face font-lock-keyword-face)
+   (propertize     (mtn-make-exercise)  'face font-lock-string-face)
+   " "
+   )
+  (let ((w (widget-create 'editable-field
+                                          :size 5
+                                          ;;                 :format "val: %v " ; Text after the field!
+                                          ""
+
+                                          
+                                          )))
+    (widget-put w
+                :action 'mtn-correct-reply)
+    (widget-put  w
+                           :answer mtn-exercise-answer))
+  (widget-insert " ")
+  (widget-setup);;
+  (widget-backward 1)
+)
+
+(defun mtn-field-edited (widget &rest ignore)
+  (message "field: %s" widget))
 
 (defun mtn-start-exercise ()
   (interactive)
@@ -75,23 +86,27 @@
   (mtn-mode)
   (let ( (inhibit-read-only t))
     (erase-buffer))
+  (remove-overlays)
+  (use-local-map widget-keymap)
   (insert "\n");;this is a workaround for thing-at-point
   (goto-char (point-min))
   (setq mtn-current-exercise 1)
   (mtn-insert-exercise)
   )
 
-(defun mtn-correct-reply ()
+(defun mtn-correct-reply (&rest args)
   (interactive)
-  (let ((ok (= mtn-exercise-answer (thing-at-point 'number))))
-    (indent-to-column 30)
-    (insert (propertize 
-             (if  ok
-                 (propertize " R \n" 'font-lock-face '(:foreground "green"))
-               (propertize " F \n" 'font-lock-faces '(:foreground "red")))
-             'read-only t 'rear-nonsticky t)))
+  (let ((ok (= (widget-get (widget-at) :answer) (string-to-number (widget-value (widget-at))))))
+    (move-end-of-line 1)
+    ;;(indent-to-column 30)
+    (widget-insert 
+                   (if  ok
+                       (propertize      " R \n"  'face font-lock-builtin-face)
+                     (propertize      " F \n"  'face font-lock-keyword-face) )
+                   )
+      (widget-setup);;
   (setq mtn-current-exercise (+ mtn-current-exercise 1))
-  (mtn-insert-exercise))
+  (mtn-insert-exercise)))
 
 (define-derived-mode mtn-mode text-mode "simple math test mode")
 
